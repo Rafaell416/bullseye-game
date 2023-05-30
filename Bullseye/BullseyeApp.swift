cf7a35e29c709f5662d0845122bd438b46419d0e
Rafael Villarreal <rvillarreal416@gmail.com>
2023-05-29 20:56:46 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Rafael Antonio Villarreal Barreto on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
