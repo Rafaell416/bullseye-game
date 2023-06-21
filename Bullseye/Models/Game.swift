b0b41d67df2410401a4d0bfd3e9027f9234a1221
Rafael Villarreal <rvillarreal416@gmail.com>
2023-06-20 23:50:45 -0500

    feat: add unit tests

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -2,13 +2,15 @@ import Foundation
 
 
 struct Game {
-	var target: Int = 37
+	var target: Int = Int.random(in: 1...100)
 	var score: Int = 0
 	var round: Int = 1
 	
 	
 	func points(sliderValue: Int) -> Int {
-		return 666
+		var difference: Int = abs(target - sliderValue)
+		let awardedPoints: Int = 100 - difference
+		return awardedPoints
 	}
 	
 }
